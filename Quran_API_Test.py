4709d688ba3c26a6d5bdc8a17818c85443b3d945
Rahim Rashid <rahimrashid06@gmail.com>
2026-06-20 17:03:29 +0000

    Added SQL Lite DB stuff

--- Quran_API_Test.py
+++ Quran_API_Test.py
@@ -1,8 +1,25 @@
 import requests
+import pandas as pd
+import sqlalchemy as db
 
 BASE_URL = 'http://api.alquran.cloud/v1/'
 
+
+#getting API data
 surah = input('Enter Surah Number: ')
 response = requests.get(BASE_URL + 'surah/' + surah)
 
 print(response.json())
+
+#converting api json to dataframe
+dataframe_name = pd.DataFrame.from_dict(response.json())
+
+#creating engine and saving to sql
+engine = db.create_engine('sqlite:///data_base_name.db')
+
+dataframe_name.to_sql('table_name', con=engine, if_exists='replace', index=False)
+
+#quering data and printing
+with engine.connect() as connection:
+   query_result = connection.execute(db.text("SELECT * FROM table_name;")).fetchall()
+   print(pd.DataFrame(query_result))
